a636e6abad74d07901fb36e978e9190f97f66810
krzysztof <krzysiek.milewski@gmail.com>
2018-10-02 22:15:08 +0200

    /add missing file to initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path: '', redirectTo: 'loading', pathMatch: 'full'},
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'header', loadChildren: './header/header.module#HeaderPageModule' },
   { path: 'tabs', loadChildren: './tabs/tabs.module#TabsPageModule' },
--- src/app/menu/menu.page.spec.ts
+++ src/app/menu/menu.page.spec.ts
@@ -1,7 +1,7 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { MenuPage } from './menu.page';
+import {MenuPage} from './menu.page';
 
 describe('MenuPage', () => {
   let component: MenuPage;
