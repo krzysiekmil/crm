3f44f117bb3438f493bd9cfa3a42dc7d65936540
krzysztof <krzysiek.milewski@gmail.com>
2018-10-02 23:50:00 +0200

    /create layout for loading

--- src/app/loading/loading.page.ts
+++ src/app/loading/loading.page.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-loading',
@@ -7,9 +8,17 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoadingPage implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
   ngOnInit() {
-  }
+      // setTimeout(() => {
+      //     return this.router.navigateByUrl('/login');
+      // }, 4500);
 
+  }
 }
+
+
+
+
